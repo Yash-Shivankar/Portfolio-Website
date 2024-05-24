3f304061e1e370eaa2153d10bbface0b7c5e87c6
Yash S <yashshivankar@outlook.com>
2024-05-24 07:24:08 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# OCTANET_JANUARY_TASK_01
+ This is a repository for the task 01 assigned by the Octanet pvt Ltd for the web development internship
---
+++ script.js
@@ -0,0 +1,45 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+document.querySelectorAll('header nav a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetSection.offsetTop - 80,
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+});
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+const typed = new Typed('.multiple-text', {
+    strings: ['CSE Student at GCOEC', 'Software developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
